9fd7e575b192c68ee250f881faca77923c06addb
pavanbca123 <patilsanchita025@gmail.com>
2025-04-22 14:33:14 +0530

    save

---
+++ Student.java
@@ -0,0 +1,16 @@
+package Branch.branch.Demojenkins;
+
+public class Student {
+    public static void main(String[] args) {
+        String name="chetan";
+        int id=7;
+        double fee =1000.00;
+        String department="bca";
+
+        System.out.println("student name:"+name);
+        System.out.println("student id:"+id);
+        System.out.println("student fee:"+fee);
+        System.out.println("student department:"+department);
+    }
+    
+}
