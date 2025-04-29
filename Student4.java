2975a3b8978763191f3931acaa43f3b778d9bd02
pavanbca123 <pavansatish201@gmail.com>
2025-04-29 16:00:09 +0530

    Update Student4.java

--- Student4.java
+++ Student4.java
@@ -1,26 +1,23 @@
 package Branch.branch.Demojenkins;
 
 public class Student4 {
-        String name;
-        int age;
-        String dob;
-    
-        
-        void details(String name, int age, String dob) {
-            this.name = name;
-            this.age = age;
-            this.dob = dob;
-    
-            
-            System.out.println("Name: " + name);
-            System.out.println("Age: " + age);
-            System.out.println("Birthdate: " + dob);
-        }
-    
-        public static void main(String[] args) {
-            Student4 s1 = new Student4(); 
-            s1.details("Chetan", 22, "27-May-2003");
-        }
-    }
-    
+    String name;
+    int age;
+    String dob;
 
+    void details(String name, int age, String dob) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+
+        // Print instance variables instead of the method parameters
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age);
+        System.out.println("Birthdate: " + this.dob);
+    }
+
+    public static void main(String[] args) {
+        Student4 s1 = new Student4(); 
+        s1.details("Chetan", 22, "27-May-2003");
+    }
+}
