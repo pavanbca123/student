73dcd88f331778a5a55b50c7d6e4e89710362858
pavanbca123 <patilsanchita025@gmail.com>
2025-04-29 14:59:12 +0530

    save

---
+++ new.java
@@ -0,0 +1,21 @@
+package Branch.branch.Demojenkins;  
+public class new {
+    public void displayCourses() {
+        System.out.println("Semester 1:");
+        System.out.println("  Course: Math - Marks: 85");
+        System.out.println("  Course: English - Marks: 90");
+
+        System.out.println("Semester 2:");
+        System.out.println("  Course: Physics - Marks: 80");
+        System.out.println("  Course: Chemistry - Marks: 88");
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("Chetan", "2004-05-27");
+        student.displayStudentInfo();
+
+        StudentCourses sc = new StudentCourses();
+        sc.displayCourses();
+    }
+}
+
